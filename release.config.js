4100535f24fb4845f46459c4d5c61449522797c6
David Ko <david.holy.ko@gmail.com>
2022-03-19 15:42:19 -0400

    feat(release): add semantic release

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'package-lock.json'],
+        message: 'semantic-release: ${nextRelease.version}',
+      },
+    ],
+  ],
+};
