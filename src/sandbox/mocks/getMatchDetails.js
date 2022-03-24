5e7b836f44ff3215f56323dae451dcbb4febeb91
David Ko <david.holy.ko@gmail.com>
2022-03-23 21:42:43 -0400

    chore(mocks): add comments to mocks

--- src/sandbox/mocks/getMatchDetails.js
+++ src/sandbox/mocks/getMatchDetails.js
@@ -1,6 +1,8 @@
 /**
  * Information from:
  * /getmatchdetails[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{match_id}
+ *
+ * Includes Information about each player in the match (upto 10)
  */
 export const mockMatchDetails = [
   {
