faa034109517b932c9c4346211bf38a90d71f24c
davidholyko <david.holy.ko@gmail.com>
2022-04-03 11:23:17 -0400

    fix(jest): fix coverage report command

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,6 @@
 const config = {
-  collectCoverageFrom: ['**/*.{js}', '!**/node_modules/**'],
-
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: -10,
-    },
-  },
+  collectCoverage: true,
+  coverageReporters: ['json', 'html'],
 
   globalSetup: './test/jestGlobalSetup.js',
 
